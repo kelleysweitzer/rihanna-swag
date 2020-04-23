5caf4cdee408b8e9d2e4f830e02d3e12edb18cb5
Kelley Sweitzer <kjsweitz@umich.edu>
2020-04-23 04:21:17 -0400

    got the handsl

--- java.js
+++ java.js
@@ -1,79 +1,34 @@
-function fetch(term1, term2, canvas, ctx){
 
-  $.ajax(
-    {
-      url: 'https://itunes.apple.com/search',
-      crossDomain: true,
-      dataType: 'jsonp',
-      data: {
-        term: term1,
-        entity: 'album',
-        limit: 1,
-        explicit: 'No'
-      },
-      method: 'GET',
-      success: function(data){
-        console.log(data);
-        albumURL =  data.results[0].artworkUrl100.replace('100x100','480x480');
-        console.log(albumURL);
-        
-    const album1 = drawalbum1(albumURL, canvas, ctx);
-      },
-      
-      error: function(e){
-        console.log(e);
-        return e;
-      }
-    }
-    
-    ); 
-  
-  
-    $.ajax(
-      {
-        url: 'https://itunes.apple.com/search',
-        crossDomain: true,
-        dataType: 'jsonp',
-        data: {
-          term: term2,
-          entity: 'album',
-          limit: 1,
-          explicit: 'No'
-        },
-        method: 'GET',
-        success: function(data){
-          console.log(data);
-          albumURL =  data.results[0].artworkUrl100.replace('100x100','480x480');
-          console.log(albumURL);
-          
-      const album2 = drawalbum2(albumURL, canvas, ctx);
-        },
-        
-        error: function(e){
-          console.log(e);
-          return e;
-        }
-      }
-      ); 
-}
 
 
 function drawBackgroundImage(canvas, ctx) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  const img = document.getElementById('rihanna');  
+  const img = document.getElementById('rihanna'); 
   ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 }
 
+function drawhands(canvas, ctx) {
+  const img = document.getElementById('hands'); 
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  return image;
+}
 
 function drawalbum2(src, canvas, ctx) {
   // Create an image object. (Not part of the dom)
   const image = new Image();
   image.src = src;
-  
+  const img = document.getElementById('hands'); 
+
   // After the image has loaded, draw it to the canvas
   image.onload = function() {
       ctx.drawImage(image, canvas.width - 280, canvas.height - 280, 220, 220);
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
   }
+  // drawhands(canvas, ctx);
+  
+  // img.onloadeddata = function () {
+  // }
   return image;
 }
 
@@ -82,11 +37,17 @@ function drawalbum1(src, canvas, ctx) {
   // Create an image object. (Not part of the dom)
   const image = new Image();
   image.src = src;
-  
+  const img = document.getElementById('hands'); 
+
   // After the image has loaded, draw it to the canvas
   image.onload = function() {
       ctx.drawImage(image, canvas.width - 730, canvas.height - 578, 220, 220);
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
   }
+  // drawhands(canvas, ctx);
+  // img. = function () {
+  // }
   return image;
 }
 
@@ -105,8 +66,6 @@ function store(){
   var artist2 = document.getElementById("artist2");
   sessionStorage.setItem("artist2", artist2.value);
   
-  // console.log( storedAlbum1 );
-  // console.log( storedAlbum2 );
   location.reload();
 
   }
@@ -117,16 +76,81 @@ onload = function() {
   const ctx = canvas.getContext('2d');
 
   drawBackgroundImage(canvas, ctx);
+
   if(sessionStorage.length == 0) {
-    
+    const hands = drawhands(canvas,ctx);
+
   } else {
     var query1 = sessionStorage.getItem("album1") + " " + sessionStorage.getItem("artist1");
     var query2 = sessionStorage.getItem("album2") + " " +sessionStorage.getItem("artist2");
     console.log(query1);
     var albumart = fetch(query1, query2, canvas, ctx);
     sessionStorage.clear();
+    // const hands = drawhands(canvas,ctx);
+
+
   }
   
+  function fetch(term1, term2, canvas, ctx){
+
+    $.ajax(
+      {
+        url: 'https://itunes.apple.com/search',
+        crossDomain: true,
+        dataType: 'jsonp',
+        data: {
+          term: term1,
+          entity: 'album',
+          limit: 1,
+          explicit: 'No'
+        },
+        method: 'GET',
+        success: function(data){
+          console.log(data);
+          albumURL =  data.results[0].artworkUrl100.replace('100x100','480x480');
+          console.log(albumURL);
+          
+      const album1 = drawalbum1(albumURL, canvas, ctx);
+ 
+
+        },
+        
+        error: function(e){
+          console.log(e);
+          return e;
+        }
+      }
+      
+      ); 
+    
+    
+      $.ajax(
+        {
+          url: 'https://itunes.apple.com/search',
+          crossDomain: true,
+          dataType: 'jsonp',
+          data: {
+            term: term2,
+            entity: 'album',
+            limit: 1,
+            explicit: 'No'
+          },
+          method: 'GET',
+          success: function(data){
+            console.log(data);
+            albumURL =  data.results[0].artworkUrl100.replace('100x100','480x480');
+            console.log(albumURL);
+            const album2 = drawalbum2(albumURL, canvas, ctx);
+
+          },
+          
+          error: function(e){
+            console.log(e);
+            return e;
+          }
+        }
+        ); 
+  }
 };
   
   
